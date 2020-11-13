c7a2f1763a3672742393a498952611db7b339f03
lucia-tellez <l.tellez.012gmail.com>
2020-11-13 12:34:11 +0000

    Exercise 1 lecture 16 done

--- Lecture16/Lecture16.py
+++ Lecture16/Lecture16.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
 
+import re
 
+AccNumbers = ["xkn59438", "yhdck2", "eihd39d9", "chdsye847", "hedle3455", "xjhd53e", "45da", "de37dp"]
+
+outputs = []
+
+for AccN in AccNumbers:
+	if re.search(r"5", AccN):
+		outputs.append(AccN + " : contains number 5")
+	if re.search(r"[de]", AccN):
+		outputs.append(AccN + " : contains the letter d or e")
+	if re.search(r"de", AccN):
+		outputs.append(AccN + " : contains adjacent d and e")
+	if re.search(r"d.*e", AccN): 
+		outputs.append(AccN + " : contains d and e in that order, doesn't have to be adjacent")
+		
+
+outputs.sort()
+print(('\n').join(outputs))
